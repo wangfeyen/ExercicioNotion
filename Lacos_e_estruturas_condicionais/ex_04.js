d6d46eb0414234d495ecd44970787a8666d77ad2
wangfeyen <wangfeyen@gmail.com>
2023-08-18 00:05:55 -0300

    primo

--- Lacos_e_estruturas_condicionais/ex_04.js
+++ Lacos_e_estruturas_condicionais/ex_04.js
@@ -1,4 +1,15 @@
 //Verifique se um número é primo ou não.
-function primo(){
-    
+function primoounao(n){
+    primo = true;
+    for(i=parseFloat(n)-1;i>1;i=i-1){
+        if(parseFloat(n)%i===0){
+            primo=false;
+        }
+    }
+    if(primo===true){
+        console.log(`${n} é primo`)
+    }else{
+        console.log(`${n} não é primo`)
+    }
 }
+primoounao(7)
